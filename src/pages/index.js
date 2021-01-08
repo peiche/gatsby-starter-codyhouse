6c8f17b895c978302d4a8b8c8df606946aeb0514
Paul Eiche <peiche@gmail.com>
2021-01-07 23:57:50 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div className="max-width-xxxs margin-y-sm">
-      <Image />
+    <div className="text-component">
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div className="max-width-xxxs margin-y-sm">
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
