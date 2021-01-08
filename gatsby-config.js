87af14f05cc21df0544cacc4ca2ae2f581a3a6f5
Paul Eiche <peiche@gmail.com>
2021-01-07 23:58:03 -0600

    Fixed name and added manifest colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,9 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-starter-codyhouse`,
         short_name: `starter`,
         start_url: `/`,
+        background_color: `#2a6df4`,
+        theme_color: `#2a6df4`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
