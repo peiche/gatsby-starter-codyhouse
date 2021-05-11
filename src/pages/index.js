5d1cf52d647a60d16952a2424d04ff089dfa5158
Paul <peiche@gmail.com>
2021-05-10 19:25:28 -0500

    Added image plugin dependency

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
