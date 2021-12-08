53cde103dc759cdcdcd93bbc357c80780e7811fe
Paul Eiche <peiche@gmail.com>
2021-12-08 16:10:48 -0600

    Added content container.

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,9 +7,11 @@ import Seo from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className="text-component">
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
   </Layout>
 )
 
